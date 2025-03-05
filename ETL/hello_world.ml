8d25e6f6f102919042692be8a1ddc0d755dddba1
leticiacb1 <leticiacb1@al.insper.edu.br>
2025-03-05 10:16:00 -0300

    fix: hello world file

--- ETL/hello_world.ml
+++ ETL/hello_world.ml
@@ -1,6 +1,6 @@
-let langs = ["OCaml"; "Rust"]
+let langs = ["OCaml"; "Rust"];;
 
 let () =
   (*let s = String.concat ", " langs in*)
   let pp_langs = Fmt.(list ~sep:(any ", ") string) in
-  Format.printf "Hello, %a!\n" pp_langs langs
+  Format.printf "Hello, %a!\n" pp_langs langs;;
